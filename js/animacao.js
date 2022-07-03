5031303e5fa9f863771d62978e945d938c5c145e
KIQ <kaiqueoliveira257@gmail.com>
2022-07-02 18:45:18 -0700

    Inserindo slick slider, alterações no js, responsivo, index.html e css

--- js/animacao.js
+++ js/animacao.js
@@ -28,3 +28,49 @@ var typed = new Typed(".typed",{
     loop : true,
     loopCount : Infinity,
 });
+
+$('.projetos').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    speed: 600,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+        breakpoint: 1916,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+        }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 704,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }
+    ]
+});
